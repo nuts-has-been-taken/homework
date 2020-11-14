6ec13e3cfbb6c7cb1f45ea28036a26dc44678cf3
carbarcha <samhoward13261@gmail.com>
2020-11-14 16:07:15 +0800

    add lat

--- MyToDoList/models.py
+++ MyToDoList/models.py
@@ -1,7 +1,9 @@
 from datetime import datetime
-from mongoengine import ( Document, StringField, DateTimeField)
+from mongoengine import ( Document, StringField, DateTimeField, ListField)
+import random
 class Order(Document):
     create_time = DateTimeField(default=datetime.now())
     content = StringField(required=True)
     address = StringField(required=True)
     status = StringField(default="收到訂單")
+    lat = ListField(default=[random.randint(0,200),random.randint(0,200)])
