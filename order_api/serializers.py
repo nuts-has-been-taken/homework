ff1dad6eb8c5aca4ac9dff45e6fe60d44b0ca75f
carbarcha <samhoward13261@gmail.com>
2020-11-20 11:32:40 +0800

    add customer & delivery

--- order_api/serializers.py
+++ order_api/serializers.py
@@ -1,6 +1,14 @@
 from rest_framework_mongoengine.serializers import DocumentSerializer
-from MyToDoList.models import Order
+from MyToDoList.models import Order,Customer,Delivery
 class OrderSerializer(DocumentSerializer):
     class Meta:
         model = Order
+        fields = '__all__'
+class CustomerSerializer(DocumentSerializer):
+    class Meta:
+        model = Customer
+        fields = '__all__'
+class DeliverySerializer(DocumentSerializer):
+    class Meta:
+        model = Delivery
         fields = '__all__'
